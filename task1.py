b786704abea57a6a0e2606df327fe8d36deb77af
pabram <piotr.abramowicz93@gmail.com>
2017-02-13 22:08:36 +0100

    small changes to task 1

--- task1.py
+++ task1.py
@@ -1,35 +1,39 @@
-def task_1(numlist):
-    # removing zeros from list as they have no effect on a battle
-    while 0 in numlist:
-        numlist.remove(0)
+def battle(numlist):
 
     evens = []
     odds = []
 
     # assigning numbers to proper lists - even and odd
     for num in numlist:
-        if num % 2 == 0:
-            evens.append((bin(num)).replace("0b", ""))
+        if num == 0:  # removing zeros as they have no effect on a battle
+            numlist.remove(num)
+        elif num % 2 == 0:
+            evens.append(int(bin(num).replace("0b", "")))
         else:
-            odds.append((bin(num)).replace("0b", ""))
+            odds.append(int(bin(num).replace("0b", "")))
 
     # main part comparing numbers of soldiers of each side
-    if counter(evens, "0") > counter(odds, "1"):
+    even_soldiers = counter(evens, "0")
+    odd_soldiers = counter(odds, "1")
+    if even_soldiers > odd_soldiers:
         print("evens win")
-    elif counter(evens, "0") < counter(odds, "1"):
+    elif even_soldiers < odd_soldiers:
         print("odds win")
     else:
         print("tie")
 
 
 def counter(numbers, soldier):
-    """Function counting soldiers for positive numbers and spies for negative numbers and returning the difference"""
+    """Function counting soldiers for positive numbers and spies
+    for negative numbers and returning the difference"""
     spies = 0
     soldiers = 0
     for number in numbers:
-        if int(number) < 0:
+        if number < 0:
+            number = str(number)
             spies += number.count(soldier)
         else:
+            number = str(number)
             soldiers += number.count(soldier)
 
     return soldiers - spies
